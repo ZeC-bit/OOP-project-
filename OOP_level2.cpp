8d146bccc93f96ea09f35c5582f36e85e261ef7c
Lee Seung Hun <74304944+ZeC-bit@users.noreply.github.com>
2022-02-28 19:45:45 +0800

    Update and rename OOP level 2 to OOP_level2.cpp

---
+++ OOP_level2.cpp
@@ -0,0 +1,31 @@
+
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+const int NAME_LEN=20;
+
+void ShowMenu(void);
+void MakeAccount(void);
+void DepositMoney(void);
+void WithdrawMoney(void);
+void ShowAllAccInfo(void);
+
+enum {MAKE=1, DEPOSIT, WITHDRAW, INQUIRE, EXIT};
+
+class Account
+{
+  private:
+    int accID;
+    int balance;
+    char * cusName;
+  
+  public:
+    Account(int ID, int money, char * name)
+      : accID(ID), balance(money)
+    {
+      cusName = new char[strlen(name)+1];
+        strcpy(cusName, name);
+  }
+  
+  
