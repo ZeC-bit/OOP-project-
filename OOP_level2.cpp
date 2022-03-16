191959d8fb7811f62c9be1a8b39b78401486f5eb
Lee Seung Hun <74304944+ZeC-bit@users.noreply.github.com>
2022-03-16 20:59:23 +0800

    Update OOP_level2.cpp
    Added ShowMenu function

--- OOP_level2.cpp
+++ OOP_level2.cpp
@@ -96,3 +96,15 @@ int main(void)
   return 0;
 }
 
+void ShowMenu(void)
+{
+  cout << "-------------Meunu-------------" << endl;
+  cout << "1. 계좌개설" << endl;
+  cout << "2. 입 금" << endl;
+  cout << "3. 출 금" << endl;
+  cout << "4. 계좌정보 전체 출력" << endl;
+  cout << "5. 프로그램 종료" << endl;
+}
+
+
+
